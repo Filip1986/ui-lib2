2e774685a42604b809399024595ea7838e909e2b
Luca Filip <filip.luca.work@gmail.com>
2026-01-22 12:06:19 +0200

    fix issues with primeng theme config

--- ui-lib2/projects/demo/src/app/app.config.ts
+++ ui-lib2/projects/demo/src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'none'
+        }
+      }
+    })
   ]
 };
